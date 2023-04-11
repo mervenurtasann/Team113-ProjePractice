c128703c7201848d9cb5cbbd18d0fc12cad5e86a
Merve Nur Tasan <127309624+mervenurtasann@users.noreply.github.com>
2023-04-11 03:06:21 +0300

    new

--- src/week08/Ogretmen.java
+++ src/week08/Ogretmen.java
@@ -4,6 +4,7 @@ import javax.management.openmbean.SimpleType;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Ogretmen {
     static Map<String, String> ogretmenlerMap = new HashMap<>();
@@ -36,15 +37,29 @@ public class Ogretmen {
             tercih = scan.nextLine();
 
             switch (tercih) {
-                case "1":
+                case "1": // Ogretmenler Listesi Yazdir
+                    ogretmenListesiYazdir();
                     break;
-                case "2":
+                case "2": // Soyisimden Ogretmen Bulma
+                    soyisimdenOgretmenBulma();
                     break;
-                case "3":
+                case "3": // Branstan Ogretmen Bulma
+                    branstanOgretmenBulma();
                     break;
-                case "4":
+                case "4": // Bilgilerini Girerek Ogretmen Ekleme
+                    ogretmenEkleme();
                     break;
-                case "5":
+                case "5": // Kimlik No Ile Kayit Silme
+                    System.out.printf("Silinecek ogretmen kimlik no giriniz : ");
+                    String silinecekOgretmen = scan.nextLine();
+
+                    String silinecekValue = ogretmenlerMap.get(silinecekOgretmen);
+
+                    String sonucValue = ogretmenlerMap.remove(silinecekOgretmen);
+                    try {
+                        boolean sonuc = sonucValue.equals(silinecekValue);
+                    } catch (Exception e) {
+                        System.out.println("Istediginiz Tc No ile ogretmen bulunamadi");                    }
                     break;
                 case "A":
                 case "a":
@@ -58,7 +73,89 @@ public class Ogretmen {
             }
 
 
-        } while (!tercih.equalsIgnoreCase("Q"));
+        } while (!tercih.equalsIgnoreCase("q"));
         Depo.projeDurdur();
+    }
+
+    public static void ogretmenEkleme() {
+        System.out.println("Tc no : ");
+        String tcNo = scan.nextLine();
+        System.out.println("Isim : ");
+        String isim = scan.nextLine();
+        System.out.println("Soyisim : ");
+        String soyisim = scan.nextLine();
+        System.out.println("Dogum Yili : ");
+        String dogumYili = scan.nextLine();
+        System.out.println("Brans : ");
+        String brans = scan.nextLine();
+
+        String eklenecekValue = isim + ", " + soyisim + ", " + dogumYili + ", " + brans;
+        ogretmenlerMap.put(tcNo, eklenecekValue);
+    }
+
+    public static void branstanOgretmenBulma() throws InterruptedException {
+        System.out.println("aradiginiz Ogretmenin bransini giriniz");
+        String istenenSoyisim = scan.nextLine();
+
+        Set<Map.Entry<String, String>> ogretmenEntrySet = ogretmenlerMap.entrySet();
+        System.out.println("============= YILDIZ KOLEJI =============\n" +
+                "======BRANS ILE OGRETMEN ARAMA ========\n" +
+                "TcNo        Isim   Soyisim   D.Yili  Brans");
+
+        for (Map.Entry<String, String> each : ogretmenEntrySet
+        ) {
+            String eachKey = each.getKey();
+            String eachValue = each.getValue();
+
+            String eachValuarr[] = eachValue.split(", ");
+            if (istenenSoyisim.equalsIgnoreCase(eachValuarr[3])) {
+                System.out.printf("%11s %-6s  %-8s %4s    %s \n", eachKey, eachValuarr[0], eachValuarr[1],
+                        eachValuarr[2], eachValuarr[3]);
+            }
+        }
+        Thread.sleep(5000);
+    }
+
+    public static void soyisimdenOgretmenBulma() throws InterruptedException {
+        System.out.println("aradiginiz Ogretmenin soyismini giriniz");
+        String istenenSoyisim = scan.nextLine();
+
+        Set<Map.Entry<String, String>> ogretmenEntrySet = ogretmenlerMap.entrySet();
+        System.out.println("============= YILDIZ KOLEJI =============\n" +
+                "======SOYISIM ILE OGRETMEN ARAMA ========\n" +
+                "TcNo        Isim   Soyisim   D.Yili  Brans");
+
+        for (Map.Entry<String, String> each : ogretmenEntrySet
+        ) {
+            String eachKey = each.getKey();
+            String eachValue = each.getValue();
+
+            String eachValuarr[] = eachValue.split(", ");
+            if (istenenSoyisim.equalsIgnoreCase(eachValuarr[1])) {
+                System.out.printf("%11s %-6s  %-8s %4s    %s \n", eachKey, eachValuarr[0], eachValuarr[1],
+                        eachValuarr[2], eachValuarr[3]);
+            }
+        }
+        Thread.sleep(5000);
+    }
+
+    public static void ogretmenListesiYazdir() throws InterruptedException {
+
+        Set<Map.Entry<String, String>> ogretmenEntrySet = ogretmenlerMap.entrySet();
+        System.out.println("============= YILDIZ KOLEJI =============\n" +
+                "=========== OGRETMEN LISTESI ============\n" +
+                "TcNo           Isim   Soyisim   Dogum Yili   Brans");
+
+        for (Map.Entry<String, String> each : ogretmenEntrySet) {
+            String eachKey = each.getKey();
+            String eachValue = each.getValue();
+
+            String eachValueArr[] = eachValue.split(",");
+            System.out.printf("%11s   %-6s  %-8s %4s      %s \n", eachKey, eachValueArr[0], eachValueArr[1],
+                    eachValueArr[2], eachValueArr[3]);
+
+        }
+        Thread.sleep(5000);
+
     }
 }
